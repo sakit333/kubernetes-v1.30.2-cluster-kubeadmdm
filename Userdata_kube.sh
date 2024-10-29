83fca0c12df45b4fadce670d4701b71d1ce6c22c
SAK <120071904+sakit333@users.noreply.github.com>
2024-10-29 13:14:43 +0530

    Update Userdata_kube.sh

--- Userdata_kube.sh
+++ Userdata_kube.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-curl -sSL https://github.com/sakit333/kubernetes-v1.30.2-cluster-kubeadmdm/blob/ec57dfb775a2061486a57458d71b43419aba4823/kube.sh | bash
+curl -O https://raw.githubusercontent.com/sakit333/kubernetes-v1.30.2-cluster-kubeadmdm/edc77307b47bcc0381d61ed363ea0a2c6621b7cc/kube.sh && chmod +x kube.sh && ./kube.sh
+
+
+or 
+
+curl -s https://raw.githubusercontent.com/sakit333/kubernetes-v1.30.2-cluster-kubeadmdm/edc77307b47bcc0381d61ed363ea0a2c6621b7cc/kube.sh | bash
+
